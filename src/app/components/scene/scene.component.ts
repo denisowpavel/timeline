97c6e6bed837b97ed3521f422db3fcec198bb19b
Pavel Denisov <denisowpavel@gmail.com>
2024-03-09 21:54:22 +0700

    simple view settings

--- src/app/components/scene/scene.component.ts
+++ src/app/components/scene/scene.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  OnInit,
+} from '@angular/core';
 import { SlotLineComponent } from './components/slot-line/slot-line.component';
 import { TimeRulerComponent } from './components/time-ruler/time-ruler.component';
 import { TlSceneView } from './types/tl-scene';
@@ -14,9 +19,31 @@ import { JsonPipe } from '@angular/common';
 })
 export class SceneComponent implements OnInit {
   public view?: TlSceneView;
-
+  @HostListener('document:mousewheel', ['$event']) onScrollEvent(
+    event: WheelEvent,
+  ): void {
+    if (!event.shiftKey && !event.altKey && event.deltaY !== 0) {
+      this.updateScale(event.deltaY);
+    }
+    if (event.deltaX !== 0 || (event.deltaY !== 0 && event.shiftKey)) {
+      this.updateStartTime(event.deltaX || event.deltaY);
+    }
+  }
   ngOnInit() {
     this.view = { scale: 1, startTime: new Date(), currentTime: new Date() };
+  }
+  updateScale(delta: number) {
+    if (!this.view) {
+      return;
+    }
+    this.view.scale += delta;
+  }
+  updateStartTime(delta: number) {
+    if (!this.view) {
+      return;
+    }
+    const timestamp = Math.floor(this.view.startTime.getTime() / 1000)
+    this.view.startTime.setTime((timestamp + delta)*1000);
   }
 
   protected readonly JsonPipe = JsonPipe;
